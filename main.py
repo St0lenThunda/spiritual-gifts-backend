d249519085655484223e870180152b758e6b9359
Antonio Moses <tonym415@gmail.com>
2025-12-07 00:13:24 -0500

    feat: Enable CORS for specified origins.

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routes.api import router as api_router
 
 app = FastAPI()
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "https://sga-v1.netlify.app",
+    "https://spiritual-gifts-backend-d82f.onrender.com"
+]
 
 @app.get("/")   
 def root():
